062fddd87bdbc437870bf0a5396310b329abf8bb
maximgorbachiov <maksimka.gorbachev.95.gmail.com>
2016-04-18 00:59:57 +0300

    Added RoomRepositoryTest.

---
+++ test/Repositories/RoomRepositoryTest.java
@@ -0,0 +1,50 @@
+package Repositories;
+
+import Model.Room;
+import Utils.DatabaseUtils;
+import Utils.TestDatabaseUtils;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+
+/**
+ * Created by maksi on 18-Apr-16.
+ */
+public class RoomRepositoryTest {
+
+    @Test
+    public void testCreateReadDelete() throws Exception {
+
+        int expectedBlockId = 1, expectedRoomNumber = 1, expectedMaxPlacesCount = 4, expectedFreePlacesCount = 2;
+
+        Room expectedRoom = new Room();
+        expectedRoom.setBlockId(expectedBlockId);
+        expectedRoom.setRoomNumber(expectedRoomNumber);
+        expectedRoom.setMaxPlacesCount(expectedMaxPlacesCount);
+        expectedRoom.setFreePlacesCount(expectedFreePlacesCount);
+
+        RoomRepository rr = new RoomRepository();
+        int last_inserted_id = rr.create(expectedRoom);
+        Room actualRoom = rr.read(last_inserted_id);
+
+        rr.delete(last_inserted_id);
+
+        Assert.assertEquals(expectedBlockId, actualRoom.getBlockId());
+        Assert.assertEquals(expectedRoomNumber, actualRoom.getRoomNumber());
+        Assert.assertEquals(expectedMaxPlacesCount, actualRoom.getMaxPlacesCount());
+        Assert.assertEquals(expectedFreePlacesCount, actualRoom.getFreePlacesCount());
+    }
+
+    @Test
+    public void testUpdateReadDelete() throws Exception {
+
+    }
+
+    @Before
+    public void createConnection() {
+        DatabaseUtils.setInstance(new TestDatabaseUtils());
+    }
+}
