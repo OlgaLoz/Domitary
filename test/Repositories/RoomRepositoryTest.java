15a5d687421dcb721189bc6307b9edb776767b8e
maximgorbachiov <maksimka.gorbachev.95.gmail.com>
2016-04-22 00:19:51 +0300

    Added update test method for roomrepository

--- test/Repositories/RoomRepositoryTest.java
+++ test/Repositories/RoomRepositoryTest.java
@@ -40,7 +40,27 @@ public class RoomRepositoryTest {
 
     @Test
     public void testUpdateReadDelete() throws Exception {
+        int expectedBlockId = 1, expectedRoomNumber = 1, expectedMaxPlacesCount = 4, freePlacesCount = 2, expectedFreePlacesCount = 1;
 
+        Room expectedRoom = new Room();
+        expectedRoom.setBlockId(expectedBlockId);
+        expectedRoom.setRoomNumber(expectedRoomNumber);
+        expectedRoom.setMaxPlacesCount(expectedMaxPlacesCount);
+        expectedRoom.setFreePlacesCount(freePlacesCount);
+
+        RoomRepository rr = new RoomRepository();
+        int last_inserted_id = rr.create(expectedRoom);
+        expectedRoom.setRoomId(last_inserted_id);
+        expectedRoom.setFreePlacesCount(expectedFreePlacesCount);
+        rr.update(expectedRoom);
+
+        Room actualRoom = rr.read(last_inserted_id);
+        rr.delete(last_inserted_id);
+
+        Assert.assertEquals(expectedBlockId, actualRoom.getBlockId());
+        Assert.assertEquals(expectedRoomNumber, actualRoom.getRoomNumber());
+        Assert.assertEquals(expectedMaxPlacesCount, actualRoom.getMaxPlacesCount());
+        Assert.assertEquals(expectedFreePlacesCount, actualRoom.getFreePlacesCount());
     }
 
     @Before
