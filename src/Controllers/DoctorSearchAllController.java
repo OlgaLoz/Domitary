e7c773434ca3807afe1124b1edc74868c5c925be
Natalia Vladimirova <dochka.tankista@gmail.com>
2016-04-23 00:11:10 +0300

    Modified doctor controllers and student repository.

--- src/Controllers/DoctorSearchAllController.java
+++ src/Controllers/DoctorSearchAllController.java
@@ -2,6 +2,7 @@ package Controllers;
 
 import Interfaces.IController;
 import Model.Student;
+import Model.StudentStatus;
 import Repositories.StudentRepository;
 import Utils.Pages;
 
@@ -15,7 +16,7 @@ public class DoctorSearchAllController implements IController {
     @Override
     public String run(HttpServletRequest request) {
         StudentRepository repository = new StudentRepository();
-        ArrayList<Student> students = repository.readAll();
+        ArrayList<Student> students = repository.readAllByStatus(StudentStatus.DeaneryPassed);
         request.getSession().setAttribute(STUDENTS_ATTRIBUTE, students);
         return Pages.HOME_DOCTOR.getPagePath();
     }
