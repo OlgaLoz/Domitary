a8fc86644dff39faf99c50d7b75ca6cdebd670c4
OlgaLoz <lozovikolga@gmail.com>
2016-04-25 04:16:14 +0300

    Createt DeaneryCheckStudentsController.

--- src/Controllers/DeaneryCheckStudentsController.java
+++ src/Controllers/DeaneryCheckStudentsController.java
@@ -2,8 +2,10 @@ package Controllers;
 
 
 import Interfaces.IController;
+import Model.Room;
 import Model.Student;
 import Model.StudentStatus;
+import Repositories.RoomRepository;
 import Repositories.StudentRepository;
 import Utils.Pages;
 
@@ -11,15 +13,51 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 
 public class DeaneryCheckStudentsController implements IController {
-    private static final String STUDENTS_STATUS = "status";
+    private static final String STUDENT = "student";
     private static final String STUDENTS_ATTRIBUTE = "students";
+    private static final String ROOMS_ATTRIBUTE = "rooms";
+    private static final String ROOM = "room";
 
     @Override
     public String run(HttpServletRequest request) {
+
+        String student =request.getParameter(STUDENT);
+        String room = request.getParameter(ROOM);
+
         StudentRepository repository = new StudentRepository();
-      //  String studentStatus = request.getParameter(STUDENTS_STATUS);
         ArrayList<Student> students = repository.readAllByStatus(StudentStatus.Candidate);
         request.getSession().setAttribute(STUDENTS_ATTRIBUTE, students);
+
+        RoomRepository rrepository = new RoomRepository();
+        ArrayList<Room> rooms = rrepository.readAll();
+        request.getSession().setAttribute(ROOMS_ATTRIBUTE, rooms);
+
+        if(student == null || room == null)
+            return Pages.DISTR_CANDIDATES.getPagePath();
+
+        for( int j = 0; j < students.size(); j++) {
+            Integer studentId  = students.get(j).getStudentId();
+            if ( studentId == Integer.parseInt(student)){
+                repository.updateStatus(studentId,StudentStatus.DeaneryPassed);
+                repository.updateRoomId(studentId, Integer.parseInt(room));
+                students.remove(j);
+                break;
+            }
+
+        }
+
+        for( int i = 0; i < rooms.size(); i++) {
+            Integer roomId  = rooms.get(i).getRoomId();
+            if (roomId == Integer.parseInt(room)){
+                rooms.get(i).setFreePlacesCount(rooms.get(i).getFreePlacesCount() - 1);
+                rrepository.updateFreePlacesCount(roomId, rooms.get(i).getFreePlacesCount());
+                if (rooms.get(i).getFreePlacesCount() == 0){
+                    rooms.remove(i);
+                }
+                break;
+            }
+        }
+
 
         return Pages.DISTR_CANDIDATES.getPagePath();
     }
