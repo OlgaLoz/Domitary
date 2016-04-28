b1e4af0b5de0571d540c80e0dfa6db0d9cf9324c
Natalia Vladimirova <dochka.tankista@gmail.com>
2016-04-28 03:32:24 +0300

    Added statement model; modified statement controller.

--- src/Controllers/StatementController.java
+++ src/Controllers/StatementController.java
@@ -1,12 +1,14 @@
 package Controllers;
 
 import Interfaces.IController;
+import Model.Statement;
 import Model.Student;
 import Model.StudentStatus;
 import Repositories.StudentRepository;
 import Utils.Pages;
 
 import javax.servlet.http.HttpServletRequest;
+import flexjson.JSONSerializer;
 
 public class StatementController implements IController {
 
@@ -21,11 +23,37 @@ public class StatementController implements IController {
             return Pages.HOME_GUEST.getPagePath();
         }
 
+        Statement statement = createStatementFromRequest(request);
+        JSONSerializer jsonSerializer = new JSONSerializer();
+        String serializedStatement = jsonSerializer.serialize(statement);
+
         int userId = (Integer)request.getSession().getAttribute(CURRENT_USER_ATTRIBUTE);
         Student student = studentRepository.getStudentByUserId(userId);
         studentRepository.updateStatus(student.getStudentId(), StudentStatus.Candidate);
+        studentRepository.updateStatement(student.getStudentId(), serializedStatement);
         request.getSession().setAttribute(STATUS_ATTRIBUTE, StudentStatus.Candidate);
 
         return Pages.HOME_STUDENT.getPagePath();
+    }
+
+    private Statement createStatementFromRequest(HttpServletRequest request) {
+        Statement statement = new Statement();
+        statement.setDean(request.getParameter("dean"));
+        statement.setGroup(request.getParameter("group"));
+        statement.setLastName(request.getParameter("lastname"));
+        statement.setFirstName(request.getParameter("firstname"));
+        statement.setMidName(request.getParameter("midname"));
+        statement.setRegion(request.getParameter("region"));
+        statement.setCity(request.getParameter("city"));
+        statement.setStreet(request.getParameter("street"));
+        statement.setHouse(request.getParameter("house"));
+        statement.setFlat(request.getParameter("flat"));
+        statement.setTraining(request.getParameter("training"));
+        statement.setMobilePhone(request.getParameter("mobile_phone"));
+        statement.setHomePhone(request.getParameter("home_phone"));
+        statement.setMother(request.getParameter("mother"));
+        statement.setFather(request.getParameter("father"));
+        statement.setFillingDate(request.getParameter("filling_date"));
+        return statement;
     }
 }
---
+++ src/Model/Statement.java
@@ -0,0 +1,56 @@
+package Model;
+
+public class Statement {
+
+    private String dean;
+    private String group;
+    private String firstName;
+    private String midName;
+    private String lastName;
+    private String region;
+    private String city;
+    private String street;
+    private String house;
+    private String flat;
+    private String training;
+    private String mobilePhone;
+    private String homePhone;
+    private String mother;
+    private String father;
+    private String fillingDate;
+
+    public String getDean() { return dean; }
+    public String getGroup() { return group; }
+    public String getFirstName() { return firstName; }
+    public String getMidName(){ return midName; }
+    public String getLastName(){ return lastName; }
+    public String getRegion(){ return region; }
+    public String getCity() { return city; }
+    public String getStreet(){ return street; }
+    public String getHouse() { return house; }
+    public String getFlat() { return flat; }
+    public String getTraining() { return training; }
+    public String getMobilePhone() { return mobilePhone; }
+    public String getHomePhone() { return homePhone; }
+    public String getMother() { return mother; }
+    public String getFather() { return father; }
+    public String getFillingDate() { return fillingDate; }
+
+    public void setDean(String dean){ this.dean = dean; }
+    public void setGroup(String group){ this.group = group; }
+    public void setFirstName(String firstName){ this.firstName = firstName; }
+    public void setMidName(String midName){ this.midName = midName; }
+    public void setLastName(String lastName){ this.lastName = lastName; }
+    public void setRegion(String region){ this.region = region; }
+    public void setCity(String city){ this.city = city; }
+    public void setStreet(String street){ this.street = street; }
+    public void setHouse(String house){ this.house = house; }
+    public void setFlat(String flat){ this.flat = flat; }
+    public void setTraining(String training){ this.training = training; }
+    public void setMobilePhone(String mobilePhone){ this.mobilePhone = mobilePhone; }
+    public void setHomePhone(String homePhone){ this.homePhone = homePhone; }
+    public void setMother(String mother){ this.mother = mother; }
+    public void setFather(String father){ this.father = father; }
+    public void setFillingDate(String fillingDate){ this.fillingDate = fillingDate; }
+
+}
