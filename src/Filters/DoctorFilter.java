7dfed8d948b0d5dfeccedede05deb06333a6baa0
OlgaLoz <lozovikolga@gmail.com>
2016-05-04 16:48:05 +0300

    Added and modified filters from the previous project.

---
+++ src/Filters/DoctorFilter.java
@@ -0,0 +1,42 @@
+package Filters;
+
+import Model.Role;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter(filterName = "DoctorFilter", urlPatterns = "/pages/doctor/*")
+public class DoctorFilter implements Filter {
+    private String CURRENT_ROLE_ATTRIBUTE = "currentRole";
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) resp;
+        HttpSession session = request.getSession(false);
+
+        if(session == null || session.getAttribute(CURRENT_ROLE_ATTRIBUTE) == null){
+            response.sendRedirect("/");
+            return;
+        }
+
+        Role currentRole = (Role) session.getAttribute(CURRENT_ROLE_ATTRIBUTE);
+        if (!Role.Doctor.equals(currentRole)) {
+            response.sendRedirect("/");
+        } else {
+            chain.doFilter(req, resp);
+        }
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+    public void destroy() {
+
+    }
+
+}
